f3f7320587746fd7f0330d9ede020268cdb43cdc
vishalb03 <vdbhand03@gmail.com>
2026-03-13 14:41:47 +0530

    second commit from sts

--- EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
 		System.out.println("First line in sts after git");
+		System.out.println("second line");
+		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
 }
